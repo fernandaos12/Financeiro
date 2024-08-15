b4a773eb3c149e4cb1a3d5bb014712e6a966d031
fernanda <fernandaos12@gmail.com>
2024-08-15 19:02:37 -0300

    Inicial App

---
+++ Financeiro/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financeiro.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+   // public DBSet<ContasPagar> ContasPagar {get;set;}
+}
